ae11bfa5c4658b214165587a5f23155da44acda6
paulh <phanyzewski@pauls-MacBook-Pro.local>
2021-09-22 09:03:14 -0600

    ch3

--- main.go
+++ main.go
@@ -3,15 +3,39 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
-func handlerFunc(w http.ResponseWriter, r *http.Request) {
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
 	fmt.Fprint(w, "<h1>Super Sweet Website</h1>")
 }
 
-func main() {
-	http.HandleFunc("/", handlerFunc)
-	fmt.Println("Starting server on :3000")
+func faq(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h2>FAQ</h2>"+
+		"<ul><li>why</li></ul>")
+}
+func contact(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "To get in touch, please send an email "+
+		"to <a href=\"mailto:support@lenslocked.com\">"+
+		"support@lenslocked.com</a>.")
+}
 
-	http.ListenAndServe(":3000", nil)
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "putting our best team together to find that for you")
+}
+
+func main() {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", home)
+	r.HandleFunc("/contact", contact)
+	r.HandleFunc("/faq", faq)
+
+	r.NotFoundHandler = http.HandlerFunc(notFound)
+	http.ListenAndServe(":3000", r)
 }
